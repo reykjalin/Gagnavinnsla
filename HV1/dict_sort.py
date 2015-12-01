1d20fcd0e127b1c7f66e992758cdac2460a778b7
reykjalin <kristofert13@ru.is>
2015-12-01 19:19:23 +0000

    New dict_sort, returns another dict

--- HV1/dict_sort.py
+++ HV1/dict_sort.py
@@ -2,4 +2,7 @@ def sort(d):
     items = [(v, k) for k, v in d.items()]
     items.sort()
     items = [(k, v) for v, k in items]
-    return items
+    sorted_dict = dict()
+    for (k,v) in items:
+        sorted_dict[k] = v
+    return sorted_dict
