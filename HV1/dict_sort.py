e770f1688a2b5ee545634c05c42629d179b18d5f
reykjalin <kristofert13@ru.is>
2015-12-02 13:03:11 +0000

    Now has value sort and dict sort, returns list

--- HV1/dict_sort.py
+++ HV1/dict_sort.py
@@ -1,5 +1,10 @@
-def sort(d):
+def value_sort(d):
     items = [(v, k) for k, v in d.items()]
     items.sort()
     items = [(k, v) for v, k in items]
+    return items
+
+def key_sort(d):
+    items = [(k,v) for k, v in d.items()]
+    items.sort()
     return items
