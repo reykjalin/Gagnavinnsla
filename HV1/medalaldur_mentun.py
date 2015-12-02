803535753f3f151e9194f1f005ec840750eccbc4
VladimirO88 <f8ter@hotmail.com>
2015-12-02 21:47:36 +0000

    búin

--- HV1/medalaldur_mentun.py
+++ HV1/medalaldur_mentun.py
@@ -4,116 +4,44 @@ import matplotlib.pyplot as plt
 import list_to_int as lti
 import numpy as np
 
-
-
-
-
 # Hver er meðalaldur háskólamenntaðs fólks, bæði karla og kvenna?
 
 Mentun_ar = ('20 til 24 ára','25 til 29 ára','30 til 49 ára','50 til 64 ára','65 til 74 ára')
-
+KynFlokkur = ('Alls','Karlar','Konur')
 AllsMentun = {}
-KarlaMentun = {}
-KvennaMentun = {}
 
-# Allir
-
-for x in range (len(Mentun_ar)):
-	for row in data.menntun:
-	    if row['Kyn'] == 'Alls' and row['Menntun'] == 'Háskólamenntun - ISCED 5, 6': 
-	    	if row['Aldursflokkur/Búseta'] == Mentun_ar[x]:  
-	      		row = dict_sort.key_sort(row)
-	      		row = lti.tuple2_toint(row)
-	      		AllsMentun[x] = row
-	      		AllsMentun[Mentun_ar[x]] = AllsMentun.pop(x)
+for kyn in range(len(KynFlokkur)):
+	for x in range (len(Mentun_ar)):
+		for row in data.menntun:
+		    if row['Kyn'] == KynFlokkur[kyn] and row['Menntun'] == 'Háskólamenntun - ISCED 5, 6': 
+		    	if row['Aldursflokkur/Búseta'] == Mentun_ar[x]:  
+		      		row = dict_sort.key_sort(row)
+		      		row = lti.tuple2_toint(row)
+		      		AllsMentun[x] = row
+		      		AllsMentun[Mentun_ar[x]] = AllsMentun.pop(x)
 
 
-x = np.zeros((len(Mentun_ar),len(AllsMentun[Mentun_ar[0]])))
-y = x.copy()
+	x = np.zeros((len(Mentun_ar),len(AllsMentun[Mentun_ar[0]])))
+	y = x.copy()
 
-for bla1 in range(len(Mentun_ar)):
-	for bla2 in range(len(AllsMentun[(Mentun_ar[bla1])])):	
-		x[bla1][bla2] = AllsMentun[Mentun_ar[bla1]][bla2][0]
-		y[bla1][bla2] = AllsMentun[Mentun_ar[bla1]][bla2][1]
+	for i in range(len(Mentun_ar)):
+		for j in range(len(AllsMentun[(Mentun_ar[i])])):	
+			x[i][j] = AllsMentun[Mentun_ar[i]][j][0]
+			y[i][j] = AllsMentun[Mentun_ar[i]][j][1]
 
-for i in range(len(Mentun_ar)):
-	plt.plot(x[i],y[i])
-	plt.title('Aldursflokkar (Allir)/ menntun')
+	for i in range(len(Mentun_ar)):
+		plt.subplot(3,1,kyn+1)	
+		plt.plot(x[i],y[i])
+		plt.title('Aldursflokkar ({})/ menntun'.format(KynFlokkur[kyn]))
 
-plt.figure(1)	
-plt.xlabel('Ár')
-plt.ylabel('Mannafjöldi')
-plt.legend((Mentun_ar),loc='upper right')
-plt.xlim(np.amin(x)-2, np.amax(x)+2)
-plt.ylim(np.amin(y)-1000,np.amax(y)+1000)
-plt.grid(True)
-AllsMentun.clear()
+	plt.xlabel('Ár')
+	plt.ylabel('Mannafjöldi')
+	plt.legend((Mentun_ar),loc='upper right')
+		
+	plt.xlim(np.amin(x)-2, np.amax(x)+2)
+	plt.ylim(np.amin(y)-1000,np.amax(y)+1000)
+	plt.grid(True)
 
-# Karlar
-
-for x in range (len(Mentun_ar)):
-	for row in data.menntun:
-	    if row['Kyn'] == 'Karlar' and row['Menntun'] == 'Háskólamenntun - ISCED 5, 6': 
-	    	if row['Aldursflokkur/Búseta'] == Mentun_ar[x]:  
-	      		row = dict_sort.key_sort(row)
-	      		row = lti.tuple2_toint(row)
-	      		AllsMentun[x] = row
-	      		AllsMentun[Mentun_ar[x]] = AllsMentun.pop(x)
-
-
-x = np.zeros((len(Mentun_ar),len(AllsMentun[Mentun_ar[0]])))
-y = x.copy()
-
-for bla1 in range(len(Mentun_ar)):
-	for bla2 in range(len(AllsMentun[(Mentun_ar[bla1])])):	
-		x[bla1][bla2] = AllsMentun[Mentun_ar[bla1]][bla2][0]
-		y[bla1][bla2] = AllsMentun[Mentun_ar[bla1]][bla2][1]
-
-for i in range(len(Mentun_ar)):
-	plt.plot(x[i],y[i])
-	plt.title('Aldursflokkar (Karlar) / menntun')
-
-plt.figure(2)	
-plt.xlabel('Ár')
-plt.ylabel('Mannafjöldi')
-plt.legend((Mentun_ar),loc='upper right')
-plt.xlim(np.amin(x)-2, np.amax(x)+2)
-plt.ylim(np.amin(y)-1000,np.amax(y)+1000)
-plt.grid(True)
-AllsMentun.clear()
-
-# Konur
-
-for x in range (len(Mentun_ar)):
-	for row in data.menntun:
-	    if row['Kyn'] == 'Konur' and row['Menntun'] == 'Háskólamenntun - ISCED 5, 6': 
-	    	if row['Aldursflokkur/Búseta'] == Mentun_ar[x]:  
-	      		row = dict_sort.key_sort(row)
-	      		row = lti.tuple2_toint(row)
-	      		AllsMentun[x] = row
-	      		AllsMentun[Mentun_ar[x]] = AllsMentun.pop(x)
-
-
-x = np.zeros((len(Mentun_ar),len(AllsMentun[Mentun_ar[0]])))
-y = x.copy()
-
-for bla1 in range(len(Mentun_ar)):
-	for bla2 in range(len(AllsMentun[(Mentun_ar[bla1])])):	
-		x[bla1][bla2] = AllsMentun[Mentun_ar[bla1]][bla2][0]
-		y[bla1][bla2] = AllsMentun[Mentun_ar[bla1]][bla2][1]
-
-for i in range(len(Mentun_ar)):
-	plt.plot(x[i],y[i])
-	plt.title('Aldursflokkar (Konur) / menntun')
-
-plt.figure(3)	
-plt.xlabel('Ár')
-plt.ylabel('Mannafjöldi')
-plt.legend((Mentun_ar),loc='upper right')
-plt.xlim(np.amin(x)-2, np.amax(x)+2)
-plt.ylim(np.amin(y)-1000,np.amax(y)+1000)
-plt.grid(True)
-AllsMentun.clear()
 
 
 plt.show()
