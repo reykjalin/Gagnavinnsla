ec914adede69eaf0e5090a69edcbd161b20946d3
reykjalin <kristofert13@ru.is>
2015-12-03 14:43:37 +0000

    startFile now works with new avg_sal

--- HV1/startFile.py
+++ HV1/startFile.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 from spurningarvladimir import aldurshop_mentun
 from read_csv import print_keys
-from avg_sal import average_sal
+import avg_sal as avg
 from spurningarhaukur import innanh_vs_utanh
 from q1_q3_munur import average_and_std_laun
 
@@ -13,7 +13,27 @@ print_keys()
 aldurshop_mentun()
 
 # Krilli + Jón
-average_sal(5)
+plt.figure(5)
+avg.plot_avg()
+plt.ylabel('Heildarlaun í þús. kr.')
+plt.xlabel('Ár')
+plt.legend(['Alls - Alls', 'Alls - Karlar', 'Alls - Konur'], loc='upper left')
+
+plt.twinx()
+
+avg.plot_ar()
+plt.ylabel('Fjöldi háskólamenntaðra einstaklinga')
+plt.legend(['Menntun - Alls', 'Menntun - Karlar', 'Menntun - Konur'], loc='center left')
+plt.title('Heildarlaun og háskólamenntun')
+
+
+plt.figure(9)
+avg.plot_skrifst()
+plt.ylabel('Heildarlaun í þús. kr.')
+plt.xlabel('Ár')
+plt.legend(['Alls', 'Karlar', 'Konur'], loc='upper left')
+plt.title('Heildarlaun skrifstofufólks')
+plt.grid()
 
 # Haukur
 innanh_vs_utanh()
