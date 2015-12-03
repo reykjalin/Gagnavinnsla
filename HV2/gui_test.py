b800830cee858239532c31c64bd3bd5f53a11097
reykjalin <kristofert13@ru.is>
2015-12-03 12:55:59 +0000

    Basic hello world GUI

--- HV2/gui_test.py
+++ HV2/gui_test.py
@@ -1,1 +1,13 @@
 # GUI test
+import kivy
+kivy.require('1.9.0')
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+    def build(self):
+        return Label(text='Hello World')
+
+if __name__ == '__main__':
+    MyApp().run()
