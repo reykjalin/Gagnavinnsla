c23be96a8086da3fb29406e74d70d6d1e4831c73
reykjalin <kristofert13@ru.is>
2015-12-07 21:19:48 +0000

    Now creates sql command file. jeij.

--- HVTWO/kristofer/readcsv.py
+++ HVTWO/kristofer/readcsv.py
@@ -52,4 +52,31 @@ for i in movies.index:
 
 
 ############################## Create sql commands ##############################
+outs = open('insert_to_movielens.sql', 'w')
+
+for i in movies.index:
+    mov = movies[movies.index == i].values[0]
+    outs.write("insert into movies (id, title, year, rating) values ('{}', '{}', '{}', {})\n".format(i, mov[0].replace("'", "''"), mov[1], mov[3]))
+
+for i in set(ratings.index):
+    rlist = ratings[ratings.index == i].values
+    for r in rlist:
+        outs.write("insert into ratings (movieid, userid, rating) values ('{}', '{}', {})\n".format(i, r[0], r[1]))
+
+for i in links.index:
+    l = links[links.index == i].values[0]
+    outs.write("insert into links (movieid, imdbid, tmdbid) values ('{}', '{}', '{}')\n".format(i, l[0], l[1]))
+
+for i in set(tags.index):
+    tlist = tags[tags.index == i].values
+    for t in tlist:
+        outs.write("insert into tags (movieid, userid, tag) values ('{}', '{}', '{}')\n".format(i, t[0], t[1]))
+
+for i in genres.keys():
+    glist = genres[i]
+    for g in glist:
+        outs.write("insert into genres (genre, movieid) values ('{}', '{}')\n".format(i, g))
+
+
+outs.close()
 ############################## Create sql commands ##############################
