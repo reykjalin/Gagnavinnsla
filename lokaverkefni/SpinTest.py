3e19827c06ccf870d1637395335a8a2e0dfe1499
nonnik14 <nonnik14@gmail.com>
2015-12-14 16:37:22 +0000

    new function for clicking

--- lokaverkefni/SpinTest.py
+++ lokaverkefni/SpinTest.py
@@ -6,6 +6,20 @@ from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 import PIL
 
+
+def onpick(event):
+
+    ind = event.ind
+    artist = event.artist
+    xdata = artist.get_xdata()
+    ydata = artist.get_ydata()
+
+
+    if ind[0] in range(len(xdata)):
+    	print('The coordinates are {}, {}'.format(xdata[ind[0]],ydata[ind[0]]))
+    return ind[0]
+
+
 # Takes the lattitutes, longitudes and countries as a lists and plots
 def plotme(lats,lons):
     count = 0
@@ -18,7 +32,7 @@ def plotme(lats,lons):
         # Make the globe more realistic
         map.bluemarble()
 
-        # compute the native map projection coordinates for contries.
+        # compute the native map projection coordinates for countries.
         x,y = map(lons,lats)
 
         # plot solar eclipse positions
@@ -29,7 +43,7 @@ def plotme(lats,lons):
         plt.clf()
         plt.close(fig)
 
-def plot2D(lats,lons,contries,year):
+def plot2D(lats,lons,countries,year):
 
 	# Making the plot fullscreen
 	fig = plt.figure(figsize=(20,9))
@@ -44,32 +58,18 @@ def plot2D(lats,lons,contries,year):
 	map.drawcountries()
 	map.drawstates()
 
-	# compute the native map projection coordinates for contries.
+	# compute the native map projection coordinates for countries.
 	x,y = map(lons,lats)
 
 	# plot filled circles at the locations of the contry.
 	map.plot(x,y,'rx', ms=5, picker=5)
 
-	for i in range(len(contries)):
-		text = plt.text(lons[i], lats[i], contries[i],fontsize=12,fontweight='bold',color='k')
+	for i in range(len(countries)):
+		text = plt.text(lons[i], lats[i], countries[i],fontsize=12,fontweight='bold',color='k')
 		text.set_visible(False)
 
-	def onpick(event):
-	    ind = event.ind
-
-	    #print('The coordinates are {}, {}'.format(lats[ind],lons[ind]))
-
-	    for i in range(len(countries)):
-	    	if ind == i:
-	    		print(countries[i])
-
-
-
 	fig.canvas.mpl_connect('pick_event', onpick)
-
 	plt.show()
-	#plt.savefig("mapframes/year{0}".format((str(year)), facecolor='k')
-	#plt.clf()
 
 
 
