4fecfc7c47d2955dc16b04455cb74ed82c3fbbec
reykjalin <kristofert13@ru.is>
2015-12-14 19:27:11 +0000

    added host to functions

--- lokaverkefni/populate_sql.py
+++ lokaverkefni/populate_sql.py
@@ -10,18 +10,18 @@ from extract_from_conflict import conflict, schema_conflict
 from extract_from_eclipse import eclipse, schema_eclipse
 
 
-def sql_create_database(user_name, password, name_of_database):
+def sql_create_database(user_name, password, name_of_database, host):
 
-	engine2 = create_engine('postgresql+psycopg2://{}:{}@localhost:5432' .format(user_name,password))
+	engine2 = create_engine('postgresql+psycopg2://{}:{}@{}:5432' .format(user_name,password,host))
 	connection2 = engine2.connect()
 	connection2.execute("commit")
 	connection2.execute('create database {};' .format(name_of_database))
 	connection2.close()
 	print('Database: {}, has been created' .format(name_of_database))
 
-def sql_populate_database(user_name, password, name_of_database):
+def sql_populate_database(user_name, password, name_of_database,host):
 
-	engine = create_engine('postgresql+psycopg2://{}:{}@localhost:5432/{}' .format(user_name,password,name_of_database))
+	engine = create_engine('postgresql+psycopg2://{}:{}@{}:5432/{}' .format(user_name,password,host,name_of_database))
 	connection = engine.connect()
 	print('Connected')
 	print('Creating tables')
