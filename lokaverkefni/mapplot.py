14770e159a2b3658da7c484ab9d42d9cbd2a5a11
reykjalin <kristofert13@ru.is>
2015-12-14 19:43:04 +0000

    fixed name of function

--- lokaverkefni/mapplot.py
+++ lokaverkefni/mapplot.py
@@ -1,6 +1,6 @@
 import SpinTest
 
-def testplot(): 
+def plottest(): 
     lats = [66, 65, 0, 34, -2]
     lons = [-17, 56, 0 ,22, -14]
     contries = ['Iceland','Iceland','Iceland','Iceland','Iceland']
