f6cf11ad8c90650a1b03eb300f51aab38065daf9
reykjalin <kristofert13@ru.is>
2015-12-14 13:44:17 +0000

    Now closes figures

--- lokaverkefni/SpinTest.py
+++ lokaverkefni/SpinTest.py
@@ -10,26 +10,26 @@ import PIL
 
 # Takes the lattitutes, longitudes and countries as a lists and plots
 def plotme(lats,lons,ymds):
-	count = 159
+    count = 0
 
-	# Rotate the earth
-	for l in range((2*count-180),180,2):
+    # Rotate the earth
+    for l in range((2*count-180),180,2):
+        fig = plt.figure(figsize=(10,10))
+        map = Basemap(projection='ortho', lat_0 = 23.4 , lon_0 = l ,resolution = 'l')
 
-		fig = plt.figure(figsize=(10,10))
-		map = Basemap(projection='ortho', lat_0 = 23.4 , lon_0 = l ,resolution = 'l')
+        # Make the globe more realistic
+        map.bluemarble()
 
-		# Make the globe more realistic
-		map.bluemarble()
+        # compute the native map projection coordinates for contries.
+        x,y = map(lons,lats)
 
-		# compute the native map projection coordinates for contries.
-		x,y = map(lons,lats)
+        # plot solar eclipse positions
+        map.plot(x,y,'rx', ms=6)
 
-		# plot solar eclipse positions
-		map.plot(x,y,'rx', ms=6)
-
-		plt.savefig("globeframes/frame{0}".format((str(count).rjust(3, "0"))), facecolor='k')
-		count += 1
-		plt.clf()
+        plt.savefig("globeframes/frame{0}".format((str(count).rjust(3, "0"))), facecolor='k')
+        count += 1
+        plt.clf()
+        plt.close(fig)
 
 
 
