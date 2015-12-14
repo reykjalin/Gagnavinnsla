1323391bf52544758d30a2fb5473e89803cf05a1
reykjalin <kristofert13@ru.is>
2015-12-14 19:22:46 +0000

    Added mapplot and spintest

---
+++ lokaverkefni/SpinTest.py
@@ -0,0 +1,72 @@
+# Til að keyra þessa skrá þarf að installa:
+# Basemap í gegnum pip
+# Pillow í gegnum pip
+
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+import PIL
+
+
+def onpick(event):
+
+    ind = event.ind
+    artist = event.artist
+    xdata = artist.get_xdata()
+    ydata = artist.get_ydata()
+
+
+    if ind[0] in range(len(xdata)):
+        print('The coordinates are {}, {}'.format(xdata[ind[0]],ydata[ind[0]]))
+    return ind[0]
+
+
+# Takes the lattitutes, longitudes and countries as a lists and plots
+def plotme(lats,lons):
+    count = 0
+
+    # Rotate the earth
+    for l in range((2*count-180),180,2):
+        fig = plt.figure(figsize=(10,10))
+        map = Basemap(projection='ortho', lat_0 = 23.4 , lon_0 = l ,resolution = 'l')
+
+        # Make the globe more realistic
+        map.bluemarble()
+
+        # compute the native map projection coordinates for countries.
+        x,y = map(lons,lats)
+
+        # plot solar eclipse positions
+        map.plot(x,y,'rx', ms=6)
+
+        plt.savefig("globeframes/frame{0}".format((str(count).rjust(3, "0"))), facecolor='k')
+        count += 1
+        plt.clf()
+        plt.close(fig)
+
+def plot2D(lats,lons,countries,year):
+
+    # Making the plot fullscreen
+    fig = plt.figure(figsize=(20,9))
+
+    map = Basemap(projection='cyl',resolution='c')
+    map.drawcoastlines()
+    map.fillcontinents(color='coral',lake_color='aqua')
+    map.drawmapboundary(fill_color='aqua')
+
+    #draw the edge of the map projection region
+    map.drawcountries()
+    map.drawstates()
+
+    # compute the native map projection coordinates for countries.
+    x,y = map(lons,lats)
+
+    # plot filled circles at the locations of the contry.
+    map.plot(x,y,'rx', ms=5, picker=5)
+
+    for i in range(len(countries)):
+        text = plt.text(lons[i], lats[i], countries[i],fontsize=12,fontweight='bold',color='k')
+        text.set_visible(False)
+
+    fig.canvas.mpl_connect('pick_event', onpick)
+    return fig
+    #plt.show()
---
+++ lokaverkefni/mapplot.py
@@ -0,0 +1,8 @@
+import SpinTest
+
+def testplot(): 
+    lats = [66, 65, 0, 34, -2]
+    lons = [-17, 56, 0 ,22, -14]
+    contries = ['Iceland','Iceland','Iceland','Iceland','Iceland']
+    year = 1945
+    return SpinTest.plot2D(lats,lons,contries,year)
