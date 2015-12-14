40c1292debe0b213e4e7ae50e74ec841f86f96be
nonnik14 <nonnik14@gmail.com>
2015-12-14 15:31:22 +0000

    For testing the maps

---
+++ lokaverkefni/mapplot.py
@@ -0,0 +1,9 @@
+import SpinTest
+
+lats = [66, 65, 0, 34, -2]
+lons = [-17, 56, 0 ,22, -14]
+contries = ['Iceland','Iceland','Iceland','Iceland','Iceland']
+year = 1945
+
+
+SpinTest.plot2D(lats,lons,contries,year)
