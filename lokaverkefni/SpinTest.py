50ae66a9cc2d5a02bdebd35c2bc1e4820c6d7586
nonnik14 <nonnik14@gmail.com>
2015-12-14 15:31:54 +0000

    adding a new function

--- lokaverkefni/SpinTest.py
+++ lokaverkefni/SpinTest.py
@@ -5,11 +5,9 @@
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 import PIL
-#from datetime import datetime
-
 
 # Takes the lattitutes, longitudes and countries as a lists and plots
-def plotme(lats,lons,ymds):
+def plotme(lats,lons):
     count = 0
 
     # Rotate the earth
@@ -30,6 +28,51 @@ def plotme(lats,lons,ymds):
         count += 1
         plt.clf()
         plt.close(fig)
+
+def plot2D(lats,lons,contries,year):
+
+	# Making the plot fullscreen
+	fig = plt.figure(figsize=(20,9))
+	ax = fig.add_subplot(111)
+
+	map = Basemap(projection='cyl',resolution='c')
+	map.drawcoastlines()
+	map.fillcontinents(color='coral',lake_color='aqua')
+	map.drawmapboundary(fill_color='aqua')
+
+	#draw the edge of the map projection region
+	map.drawcountries()
+	map.drawstates()
+
+	# compute the native map projection coordinates for contries.
+	x,y = map(lons,lats)
+
+	# plot filled circles at the locations of the contry.
+	map.plot(x,y,'rx', ms=5, picker=5)
+
+	for i in range(len(contries)):
+		text = plt.text(lons[i], lats[i], contries[i],fontsize=12,fontweight='bold',color='k')
+		text.set_visible(False)
+
+	def onpick(event):
+	    ind = event.ind
+
+	    #print('The coordinates are {}, {}'.format(lats[ind],lons[ind]))
+
+	    for i in range(len(countries)):
+	    	if ind == i:
+	    		print(countries[i])
+
+
+
+	fig.canvas.mpl_connect('pick_event', onpick)
+
+	plt.show()
+	#plt.savefig("mapframes/year{0}".format((str(year)), facecolor='k')
+	#plt.clf()
+
+
+
 
 
 
