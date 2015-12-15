f2c52b1c8b783638a54b4f3ef54ffde8b64c35da
nonnik14 <nonnik14@gmail.com>
2015-12-15 18:01:08 +0000

    New function get_conflicts3D that makes frames and then video

--- lokaverkefni/libs/get_data.py
+++ lokaverkefni/libs/get_data.py
@@ -1,12 +1,18 @@
 from libs.execute_query import run_query
 from libs.get_coords import get_locs_db, get_coords_from_loc_db, get_eclipse_coords
-from libs.SpinTest import plot2D
+from libs.SpinTest import plot2D, plotme
 
 def get_conflicts(engine, year):
     Cdata = get_locs_db(engine, 'conflict', 'where year = {}'.format(year))
     Clats, Clons = get_coords_from_loc_db(engine, Cdata, 'conflictlc')
     Elats, Elons = get_eclipse_coords(engine, year)
     return plot2D(Elats,Elons,True,Clats,Clons,True)
+
+def get_conflicts3D(engine, year):
+    Cdata = get_locs_db(engine, 'conflict', 'where year = {}'.format(year))
+    Clats, Clons = get_coords_from_loc_db(engine, Cdata, 'conflictlc')
+    Elats, Elons = get_eclipse_coords(engine, year)
+    plotme(Elats,Elons,True,Clats,Clons,True)
     
     
 def get_maxyear(engine):
