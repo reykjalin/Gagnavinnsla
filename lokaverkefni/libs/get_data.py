3803e2ec6c71819b8295894c2e855eb902efce63
nonnik14 <nonnik14@gmail.com>
2015-12-15 11:41:44 +0000

    Adding eclipse plot

--- lokaverkefni/libs/get_data.py
+++ lokaverkefni/libs/get_data.py
@@ -3,9 +3,13 @@ from libs.get_coords import get_coords
 from SpinTest import plot2D
 
 def get_conflicts(engine, year):
-    data = run_query(engine, 'distinct location', 'conflict', 'where year = {}'.format(year))
-    lats, lons = get_coords(data)
-    return plot2D(lats, lons)
+    Cdata = run_query(engine, 'distinct location', 'conflict', 'where year = {}'.format(year))
+    Clats, Clons = get_coords(Cdata)
+
+    Elats = run_query(engine, 'lat', 'eclipse', 'where year = {}'.format(year))
+    Elons = run_query(engine, 'long', 'eclipse', 'where year = {}'.format(year))
+    
+    return plot2D(Elats,Elons,True,Clats,Clons,True)
     
 def get_maxyear(engine):
     return run_query(engine, 'max(year)', 'conflict').fetchall()[0][0]
